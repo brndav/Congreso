74f839cbdcc153f729dc4ef44f3b2cd4c13cbd5b
Maxialfre <113524078+Maxialfre@users.noreply.github.com>
2024-02-29 10:52:13 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontCong',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
